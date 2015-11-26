0988ea3f85130ae121afcc9d83c6db074362de5f
flada-auxv <aseknock@gmail.com>
2015-11-27 00:42:20 +0900

    custom fmt for Tile

--- src/janjanjan/tile.rs
+++ src/janjanjan/tile.rs
@@ -1,5 +1,7 @@
+use std::fmt;
+
 #[cfg_attr(rustfmt, rustfmt_skip)]
-#[derive(Debug,Clone)]
+#[derive(Clone)]
 pub enum Tile {
     Pin1, Pin2, Pin3, Pin4, Pin5, Pin6, Pin7, Pin8, Pin9,
     Sou1, Sou2, Sou3, Sou4, Sou5, Sou6, Sou7, Sou8, Sou9,
@@ -58,5 +60,47 @@ impl Tile {
         let mut ret = Tile::all_tiles();
         rng.shuffle(&mut ret);
         ret
+    }
+}
+impl fmt::Debug for Tile {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        use tile::Tile::*;
+
+        match *self {
+            Pin1 => write!(f, "1p"),
+            Pin2 => write!(f, "2p"),
+            Pin3 => write!(f, "3p"),
+            Pin4 => write!(f, "4p"),
+            Pin5 => write!(f, "5p"),
+            Pin6 => write!(f, "6p"),
+            Pin7 => write!(f, "7p"),
+            Pin8 => write!(f, "8p"),
+            Pin9 => write!(f, "9p"),
+            Sou1 => write!(f, "1s"),
+            Sou2 => write!(f, "2s"),
+            Sou3 => write!(f, "3s"),
+            Sou4 => write!(f, "4s"),
+            Sou5 => write!(f, "5s"),
+            Sou6 => write!(f, "6s"),
+            Sou7 => write!(f, "7s"),
+            Sou8 => write!(f, "8s"),
+            Sou9 => write!(f, "9s"),
+            Wan1 => write!(f, "1w"),
+            Wan2 => write!(f, "2w"),
+            Wan3 => write!(f, "3w"),
+            Wan4 => write!(f, "4w"),
+            Wan5 => write!(f, "5w"),
+            Wan6 => write!(f, "6w"),
+            Wan7 => write!(f, "7w"),
+            Wan8 => write!(f, "8w"),
+            Wan9 => write!(f, "9w"),
+            WindTon => write!(f, "東"),
+            WindNan => write!(f, "南"),
+            WindSha => write!(f, "西"),
+            WindPei => write!(f, "北"),
+            DragonHaku => write!(f, "白"),
+            DragonHatsu => write!(f, "發"),
+            DragonChun => write!(f, "中"),
+        }
     }
 }
